9ab49c0d447224a18462fb8d8d0505077f7d791b
amit <amit@inceptioncloud.io>
2020-10-07 05:59:08 +0000

    changes for libinceptionappagentproc.so and correct path as per inception operator

--- execve_so/build.sh
+++ execve_so/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -std=c99 -o exec.so -shared java_exec.c -Wall -Wfatal-errors -fPIC -g -ldl
+gcc -std=c99 -o libinceptionappagentproc.so -shared java_exec.c -Wall -Wfatal-errors -fPIC -g -ldl
--- execve_so/java_exec.c
+++ execve_so/java_exec.c
@@ -5,7 +5,7 @@
  * Example:
  * origional command: java -jar HelloWorld-all-1.0.jar
  *
- * modified by command: java -javaagent:HelloWorldAgent-1.0-SNAPSHOT.jar -jar HelloWorld-all-1.0.jar
+ * modified by command: java -javaagent:/opt/inception/inceptionappagent-paas/agent/java/inception-javaagent.jar -jar HelloWorld-all-1.0.jar
  *
  * compile command: gcc -std=c99 -o exec.so -shared java_exec.c -Wall -Wfatal-errors -fPIC -g -ldl
  *
@@ -21,23 +21,23 @@
 
 typedef ssize_t (*execve_func_t)(const char* filename, char* const argv[], char* const envp[]);
 static execve_func_t old_execve = NULL;
-char* const javaAgent = "-javaagent:HelloWorldAgent-1.0-SNAPSHOT.jar"; 
+char* const javaAgent = "-javaagent:/opt/inception/inceptionappagent-paas/agent/java/inception-javaagent.jar"; 
 int execve(const char* filename, char* const argv[], char* const envp[]) {
-	printf("Running hook for Java Agent\n");
+	printf("LD_PRELOAD: Running hook for Java Agent\n");
 	int i;  
 	for (i = 0 ; argv[i] != NULL; i++){
 	}
 	int sizeArgv = i +1;
-	if(strcmp(argv[0], "java")){
+	int isNotJava = strcmp(argv[0], "java");
+	if (isNotJava) {
 		old_execve = dlsym(RTLD_NEXT, "execve");
 		return old_execve(filename, argv, envp);
 	}
 
-	printf("java process is starting, changing the command line arguments");
+	printf("LD_PRELOAD: Java process is starting, changing the command line arguments");
 	char * argv2[ sizeArgv + 1];
 	argv2[0] = argv[0];
 	argv2[1] = javaAgent;
-	//argv2[1] = "-javaagent:HelloWorldAgent-1.0-SNAPSHOT.jar";
 	int j = 1;
 	while(argv[j] !=NULL)
 	{
